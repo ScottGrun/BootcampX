044294a54ad17c74d4e5c61904bf017c46093a63
Scott Grunerud <sgrunerud@edu.uwaterloo.ca>
2020-08-19 20:59:55 +0000

    Added paramiterized queries

--- 4_queries/assistance-req-data.sql
+++ 4_queries/assistance-req-data.sql
@@ -1,6 +1,27 @@
+const { Pool } = require('pg');
+//const chort_name = process.argv.slice(2)[0];
+
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'bootcampx'
+});
+
+pool.query(`
 SELECT teachers.name as teacher, students.name as student, assignments.name as assignment,  (completed_at-started_at) as duration
 FROM assistance_requests
 JOIN students ON students.id = student_id
 JOIN teachers ON teachers.id = teacher_id
 JOIN assignments ON assignments.id = assignment_id
 ORDER BY duration;
+
+`)
+.then(res => {
+  res.rows.forEach(user => {
+    console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
+  })
+}).catch(err => console.error('query error', err.stack));
+
+
--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -1,13 +1,13 @@
-// const { Pool } = require('pg');
-// //const chort_name = process.argv.slice(2)[0];
+const { Pool } = require('pg');
+//const chort_name = process.argv.slice(2)[0];
 
 
-// const pool = new Pool({
-//   user: 'vagrant',
-//   password: '123',
-//   host: 'localhost',
-//   database: 'bootcampx'
-// });
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'bootcampx'
+});
 
 // pool.query(`
 // SELECT students.id as student_id, students.name as name, cohorts.name as cohort
@@ -22,29 +22,42 @@
 //   })
 // }).catch(err => console.error('query error', err.stack));
 
-const { Pool } = require('pg');
-//const chort_name = process.argv.slice(2)[0];
+// const { Pool } = require('pg');
+// //const chort_name = process.argv.slice(2)[0];
 
 
-const pool = new Pool({
-  user: 'vagrant',
-  password: '123',
-  host: 'localhost',
-  database: 'bootcampx'
-});
+// const pool = new Pool({
+//   user: 'vagrant',
+//   password: '123',
+//   host: 'localhost',
+//   database: 'bootcampx'
+// });
 
-pool.query(`
-SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
-FROM teachers
-JOIN assistance_requests ON teacher_id = teachers.id
-JOIN students ON student_id = students.id
-JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
-ORDER BY teacher;
-`)
-.then(res => {
-  res.rows.forEach(row => {
-    console.log(`${row.cohort}: ${row.teacher}`);
-  })
-});
+// pool.query(`
+// SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+// FROM teachers
+// JOIN assistance_requests ON teacher_id = teachers.id
+// JOIN students ON student_id = students.id
+// JOIN cohorts ON cohort_id = cohorts.id
+// WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
+// ORDER BY teacher;
+// `)
+// .then(res => {
+//   res.rows.forEach(row => {
+//     console.log(`${row.cohort}: ${row.teacher}`);
+//   })
+// });
 
+const queryString = `
+  SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+  FROM students
+  JOIN cohorts ON cohorts.id = cohort_id
+  WHERE cohorts.name LIKE $1
+  LIMIT $2;
+  `;
+
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+// Store all potentially malicious values in an array. 
+const values = [`%${cohortName}%`, limit];
+pool.query(queryString, values);
